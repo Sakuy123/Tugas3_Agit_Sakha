69eea017eae470ce99b8d86da0f5ba898446ac05
Sakha Ganteng Kiw Kiw <138842571+Sakuy123@users.noreply.github.com>
2024-06-21 14:24:20 +0700

    Add files via upload

---
+++ tugasagit_sakha/src/main/java/id/co/astratech/tugasagit_sakha/TugasagitSakhaApplication.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugasagit_sakha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasagitSakhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TugasagitSakhaApplication.class, args);
+    }
+
+}
