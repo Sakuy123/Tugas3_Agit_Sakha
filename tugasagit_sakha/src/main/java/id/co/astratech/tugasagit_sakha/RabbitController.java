bca6dcb193b7e6259a9786b2d514b10b17f06dca
Sakha Ganteng Kiw Kiw <138842571+Sakuy123@users.noreply.github.com>
2024-06-21 13:57:36 +0700

    Add files via upload

---
+++ tugasagit_sakha/src/main/java/id/co/astratech/tugasagit_sakha/RabbitController.java
@@ -0,0 +1,31 @@
+package id.co.astratech.tugasagit_sakha;
+
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RabbitController {
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public RabbitController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @PostMapping("/publish")
+    public String publishMessage(@RequestBody String message) {
+        rabbitTemplate.convertAndSend("sakhaExchange", "sakha061", message);
+        return "Message sent to RabbitMQ: " + message;
+    }
+
+    @RabbitListener(queues = "sakhaQueue")
+    public void processMessage(String message) {
+        System.out.println("Pesan dari sakaKuEWE: " + message);
+        // Lakukan pemrosesan pesan di sini sesuai kebutuhan Anda
+    }
+}
---
+++ tugasagit_sakha/src/test/java/id/co/astratech/tugasagit_sakha/TugasagitSakhaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugasagit_sakha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasagitSakhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
